ab7b7a10ebb572810169aaa194ac92dc89d8f2df
Kathiravan Harikrishnan <kathiravan.iosdev@gmail.com>
2025-05-11 09:41:57 +0530

    Initial Commit

---
+++ CICD/CICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDApp.swift
+//  CICD
+//
+//  Created by Kathiravan H on 11/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CICDTests/CICDTests.swift
@@ -0,0 +1,17 @@
+//
+//  CICDTests.swift
+//  CICDTests
+//
+//  Created by Kathiravan H on 11/05/25.
+//
+
+import Testing
+@testable import CICD
+
+struct CICDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
